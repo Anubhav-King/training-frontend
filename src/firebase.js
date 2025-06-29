8d7324351d27e8ebac98c958e563f58daa72e81b
Anubhav-King <awasthianubhav924@gmail.com>
2025-06-29 05:47:17 +0000

    API for OTP

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "  ",
-  authDomain: " trainingapp-3d465.firebaseapp.com ",
+  apiKey: "AIzaSyA4JTCdISIqnTc8M3PX8xAuTkyz0HujwXQ",
+  authDomain: " trainingapp-3d465.firebaseapp.com",
   projectId: " trainingapp-3d465",
-  storageBucket: " trainingapp-3d465.firebasestorage.app ",
+  storageBucket: " trainingapp-3d465.firebasestorage.app",
   messagingSenderId: "472813194782",
-  appId: "1:472813194782:web:57a41ccb63630ba8aaa80f ",
+  appId: "1:472813194782:web:57a41ccb63630ba8aaa80f",
 };
 
 const app = initializeApp(firebaseConfig);
